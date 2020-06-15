e520a06518d9d37a98f09165257abb8cee845992
Cyrux <cyl2ux@gmail.com>
2020-06-15 13:29:50 -0400

    Populate RecyclerView with placeholder data

--- app/src/main/java/mx/com/cyrux/retrofit/MainActivity.java
+++ app/src/main/java/mx/com/cyrux/retrofit/MainActivity.java
@@ -1,14 +1,21 @@
 package mx.com.cyrux.retrofit;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView rvRepositories;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rvRepositories = findViewById(R.id.rvRepositories);
+        rvRepositories.setLayoutManager(new LinearLayoutManager(this));
+        rvRepositories.setAdapter(new RepositoryAdapter(Repository.getRepositories()));
     }
 }

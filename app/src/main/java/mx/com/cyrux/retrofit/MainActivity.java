8a33eb098093d07d24adb58508aa7cf844e5ce67
Cyrux <cyl2ux@gmail.com>
2020-06-15 15:41:19 -0400

    Populate RecyclerView

--- app/src/main/java/mx/com/cyrux/retrofit/MainActivity.java
+++ app/src/main/java/mx/com/cyrux/retrofit/MainActivity.java
@@ -1,10 +1,17 @@
 package mx.com.cyrux.retrofit;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView rvRepositories;
@@ -16,6 +23,22 @@ public class MainActivity extends AppCompatActivity {
 
         rvRepositories = findViewById(R.id.rvRepositories);
         rvRepositories.setLayoutManager(new LinearLayoutManager(this));
-        rvRepositories.setAdapter(new RepositoryAdapter(Repository.getRepositories()));
+
+        GitHubService service = ServiceGenerator.createService(GitHubService.class);
+
+        Call<List<Repository>> call = service.reposForUser("cyl2ux");
+        call.enqueue(new Callback<List<Repository>>() {
+            @Override
+            public void onResponse(Call<List<Repository>> call, Response<List<Repository>> response) {
+                if (response.isSuccessful()) {
+                    rvRepositories.setAdapter(new RepositoryAdapter(response.body()));
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Repository>> call, Throwable t) {
+                Log.e("Request failed", "Cannot Request GitHub repositories");
+            }
+        });
     }
 }
